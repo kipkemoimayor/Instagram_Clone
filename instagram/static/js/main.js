444301f38481329e1b5281d4211fcc8c306610ca
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-17 23:12:05 +0300

    Added a route for loging out users

--- instagram/static/js/main.js
+++ instagram/static/js/main.js
@@ -1,51 +1,28 @@
 $(document).ready(function () {
-  $("#update").click(function(){
+  $("#prompt").click(function(){
     Swal.fire({
-    type: 'success',
-    title: 'Success',
-    text: 'Profile Updated ',
-    timer:2000,
-    showConfirmButton:false
-  })
-  })
-  $("#delete").click(function(){
-    alert("are you sure")
-    Swal.fire({
-    type: 'success',
-    title: 'Deleted',
-    text: 'Your blog has been deleted',
-    showConfirmButton:false
+    showConfirmButton:false,
+    html:
+    '<ul class="list-group"> <li class="list-group-item"><a class="ref" href="">Cancel</a> </li></ul>'+
+    '<ul class="list-group"> <li class="list-group-item"><a class="ref" href="/logout">Logout</a> </li></ul>'+
+    '<ul class="list-group"> <li class="list-group-item"><a class="ref" href="/accounts/password/change">Change Password</a></li></ul>',
   })
 })
-  $(".profile").hide()
-  $("#change").show()
-  /*business logig*/
-
-  $("#dele").click(function(){
-    $(".del").show()
-    $("#dele").hide()
-
-
-
-  })
-  $("#change").click(function(){
-    $(".profiles").show()
-    $("#change").hide()
-  })
-  $("#update").click(function(){
-    $("#change").show()
-    $(".profiles").hide()
-  })
-
-  $("#write").click(function(){
-
-    $(".com").slideDown(2000)
-      $(".com").show()
-
-  })
-
-
-
-
-
 })
+
+// $(document).ready(function(){
+//   $("#prompt").click(function(){
+//     swal({
+//       button:false,
+//       text:"heu",
+//       className:'red-bg',
+//       closeOnEsc:true,
+//       '<div>
+//       <a href="">Login<a>
+//       </div>'
+//
+//
+//     })
+//
+//   })
+// })
